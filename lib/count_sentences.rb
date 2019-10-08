cd44899a1216abfe463d2f94ae3c9f08ac61c4a5
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-10-08 20:00:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-  self.split (sentence?, question?, exclamation?)
+  self.split (self.sentence?, self.question?, self.exclamation?)
   binding.pry
   end
 end
